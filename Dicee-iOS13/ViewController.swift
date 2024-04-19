d4624543cff348856d690f4b5a183e19becc5f9a
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2024-04-19 12:08:33 -0500

    Nice!

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -6,13 +6,26 @@
 //  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
-import UIKit
+import UIKit 
 
 class ViewController: UIViewController {
 
+    // IBOulet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // WHO          WHAT       VALUE
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix") // Shift + cmd + L then Drag and Drop here.
+        diceImageView1.alpha = 0.5 // Here, we change the alpha, which is the transparency.
+        
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        diceImageView2.alpha = 0.5
+        
     }
 
 
