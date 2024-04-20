62130ecb0c2b813d8f37060c1819044a004cd102
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2024-04-20 14:37:59 -0500

    Nice!

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -15,19 +15,37 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         // WHO          WHAT       VALUE
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix") // Shift + cmd + L then Drag and Drop here.
-        diceImageView1.alpha = 0.5 // Here, we change the alpha, which is the transparency.
+        // diceImageView1.image = #imageLiteral(resourceName: "DiceSix") // Shift + cmd + L then Drag and Drop here.
+        // diceImageView1.alpha = 0.5 // Here, we change the alpha, which is the transparency.
         
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-        diceImageView2.alpha = 0.5
+        // diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        // diceImageView2.alpha = 0.5
+        
+        
         
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        // diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        // diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        leftDiceNumber += 1
+        
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        rightDiceNumber -= 1
+        
+        
+    }
+    
 }
 
