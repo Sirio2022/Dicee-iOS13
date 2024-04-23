fede1a40d1eb189c13f95b1a00ce7e62c3ea25dc
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2024-04-23 10:47:30 -0500

    Nice!

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -15,37 +15,43 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
+//    var leftDiceNumber = 1
+//    var rightDiceNumber = 5
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        // WHO          WHAT       VALUE
-        // diceImageView1.image = #imageLiteral(resourceName: "DiceSix") // Shift + cmd + L then Drag and Drop here.
-        // diceImageView1.alpha = 0.5 // Here, we change the alpha, which is the transparency.
-        
-        // diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-        // diceImageView2.alpha = 0.5
-        
-        
-        
-    }
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//        
+//        // WHO          WHAT       VALUE
+//        // diceImageView1.image = #imageLiteral(resourceName: "DiceSix") // Shift + cmd + L then Drag and Drop here.
+//        // diceImageView1.alpha = 0.5 // Here, we change the alpha, which is the transparency.
+//        
+//        // diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+//        // diceImageView2.alpha = 0.5
+//        
+//        
+//        
+//    }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
         // diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
         // diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        // In the random you can simple create a randon wich have the exacto number of items i the array.
+        // This was the case Int.random(in: 0...5), but you can use randomElement
         
-        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
-        leftDiceNumber += 1
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
-        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
-        rightDiceNumber -= 1
+        diceImageView1.image = diceArray.randomElement()
+//        leftDiceNumber += 1
         
+        diceImageView2.image = diceArray.randomElement()
+//        rightDiceNumber -= 1
         
+      
     }
+    
     
 }
 
